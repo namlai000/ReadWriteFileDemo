4aef703b09576e12fbca1aa6ef78b7ddd2603095
DragonOfJustice <theguncrusher@gmail.com>
2016-07-22 13:58:09 +0700

    Initial Project

---
+++ Demo1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string root = "D:\\";
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("1. Create a folder");
+                Console.WriteLine("2. Create a text file");
+                Console.WriteLine("3. Write to file");
+                Console.WriteLine("4. Read from file");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose: ");
+                int menu = int.Parse(Console.ReadLine());
+                switch (menu)
+                {
+                    case 1:
+                        DirectoryInfo dir = new DirectoryInfo(root);
+                        Console.Write("Enter folder name: ");
+                        string folder = Console.ReadLine();
+                        DirectoryInfo d = dir.CreateSubdirectory(folder);
+                        Console.WriteLine("Your folder is created");
+                        break;
+                    case 2:
+                        Console.Write("Enter file name: ");
+                        string file = Console.ReadLine();
+                        FileStream fs = File.Open(root + file, FileMode.Create);
+                        Console.WriteLine("Your file is created");
+                        break;
+                    case 3:
+                        Console.Write("Enter your file name: ");
+                        string file1 = Console.ReadLine();
+                        FileInfo f = new FileInfo(root + file1);
+                        if (f.Exists)
+                        {
+                            StreamWriter writer = new StreamWriter(f.FullName, true);
+                            Console.Write("Enter your message: ");
+                            string msg = Console.ReadLine();
+                            writer.WriteLine(msg);
+                            writer.Close();
+                        }
+                        else
+                        {
+                            Console.WriteLine("File not exist");
+                        }
+                        break;
+                    case 4:
+                        Console.Write("Enter your file name: ");
+                        string file2 = Console.ReadLine();
+                        FileInfo f1 = new FileInfo(root + file2);
+                        if (f1.Exists)
+                        {
+                            StreamReader reader = new StreamReader(f1.FullName);
+                            Console.WriteLine("Your message: ");
+                            string input = null;
+                            while ((input = reader.ReadLine()) != null)
+                            {
+                                Console.WriteLine(input);
+                            }
+                            reader.Close();
+                        }
+                        else
+                        {
+                            Console.WriteLine("File not exist");
+                        }
+                        break;
+                    default:
+                        loop = false;
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- ReadWriteFileDemo/Program.cs
+++ ReadWriteFileDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace ReadWriteFileDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Fun with Directiry");
+            DirectoryInfo dir = new DirectoryInfo("D:\\Source\\CNW\\Final - NetWork");
+            Console.WriteLine("Full name: {0}", dir.FullName);
+            Console.WriteLine("Name: {0}", dir.Name);
+            
+
+            FileInfo[] bitmapFiles = dir.GetFiles("*.png");
+            Console.WriteLine("Found {0} png files", bitmapFiles.Length);
+
+            foreach (FileInfo f in bitmapFiles)
+            {
+                Console.WriteLine("Filename: {0}", f.Name);
+            }
+
+            DirectoryInfo d = dir.CreateSubdirectory("MyFoo");
+            Console.ReadLine();
         }
     }
 }
